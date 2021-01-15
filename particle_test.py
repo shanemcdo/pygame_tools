f4e0283735460518080abcd422aa32406f361e32
Shane McDonough <shanemcdman@gmail.com>
2021-01-14 20:02:09 -0800

    Impliment decrement timer

--- particle_test.py
+++ particle_test.py
@@ -8,7 +8,7 @@ class ParticleTest(GameScreen):
         size = Point(600, 600)
         super().__init__(pygame.display.set_mode(size), size, (size.x // 2, size.y // 2))
         self.center = Point(self.window_size.x // 2, self.window_size.y // 2)
-        self.particles = [Particle(self.center, 30, 'grey', (1, 0), None, 1, 10)]
+        self.particles = [Particle(self.center, 10, 'grey', (1, 0), None, 1, 4)]
 
     def update(self):
         super().update()
