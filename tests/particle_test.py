f671743a16c73c51172bf34d26660227fee3788e
Shane McDonough <shanemcdman@gmail.com>
2022-04-27 10:48:53 -0700

    Update particles test

--- tests/particle_test.py
+++ tests/particle_test.py
@@ -11,7 +11,7 @@ class ParticleTest(GameScreen):
         size = Point(600, 600)
         super().__init__(pygame.display.set_mode(size), size, (size.x // 2, size.y // 2))
         self.center = Point(self.window_size.x // 2, self.window_size.y // 2)
-        self.particles = [Particle(self.center, 5, 'grey', (1, -3), None, 1, 4)]
+        self.particles = []
 
     def update(self):
         super().update()
@@ -21,6 +21,8 @@ class ParticleTest(GameScreen):
                 self.particles.remove(self.particles[i])
             else:
                 self.particles[i].update()
+                # gravity
+                self.particles[i].velocity.y += 9.8 / 50
                 self.particles[i].draw(self.screen)
                 i += 1
         self.add_particle()
